63f5bc961286dd42decbc871e90eae57b5a7fba6
Kaja <k.chade@livechatinc.com>
2022-06-20 08:48:14 +0200

    first commit simple ticket form with react

---
+++ src/components/fetchTicket.js
@@ -0,0 +1,54 @@
+const url = 'https://api.labs.helpdesk.com/v1/externalTickets';
+const licenseID = 25;
+const teamID = '29be9540-b932-4959-b8c3-f0acede85a73';
+
+const createTicket = async (
+  requesterName,
+  requesterEmail,
+  subject,
+  requesterMessage
+) => {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        licenseID: licenseID,
+        requester: {
+          name: requesterName,
+          email: requesterEmail,
+        },
+        subject: subject,
+        message: {
+          richTextObj: [
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  text: requesterMessage,
+                },
+              ],
+            },
+          ],
+        },
+        status: 'pending',
+        assignment: {
+          agent: null,
+          team: {
+            ID: teamID,
+          },
+        },
+      }),
+    });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      console.log(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { createTicket };
---
+++ src/components/ticketForm.js
@@ -0,0 +1,105 @@
+import { React, useState } from 'react';
+import { createTicket } from './fetchTicket';
+
+function TicketForm() {
+  const [requesterName, setrequesterName] = useState(null);
+  const [requesterEmail, setEmail] = useState(null);
+  const [subject, setSubject] = useState(null);
+  const [requesterMessage, setMessage] = useState(null);
+
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    if (id === 'requesterName') {
+      setrequesterName(value);
+    }
+    if (id === 'requesterEmail') {
+      setEmail(value);
+    }
+    if (id === 'subject') {
+      setSubject(value);
+    }
+    if (id === 'requesterMessage') {
+      setMessage(value);
+    }
+  };
+
+  const handleSubmit = () => {
+    console.log(requesterName, requesterEmail, subject, requesterMessage);
+
+    return createTicket(
+      requesterName,
+      requesterEmail,
+      subject,
+      requesterMessage
+    );
+  };
+
+  return (
+    <div className="form">
+      <div className="form-body">
+        <div className="username">
+          <label className="form__label" for="requesterName">
+            Full Name{' '}
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            id="requesterName"
+            placeholder="Your Name"
+            onChange={(e) => handleInputChange(e)}
+          />
+        </div>
+        <div className="email">
+          <label className="form__label" for="email">
+            Email{' '}
+          </label>
+          <input
+            type="email"
+            id="requesterEmail"
+            className="form__input"
+            placeholder="Email"
+            onChange={(e) => handleInputChange(e)}
+          />
+        </div>
+        <div className="subject">
+          <label className="form__label" for="subject">
+            Subject{' '}
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            id="subject"
+            placeholder="Subject"
+            onChange={(e) => handleInputChange(e)}
+          />
+        </div>
+        <div className="requester-message">
+          <label className="form__label" for="message">
+            Type Message{' '}
+          </label>
+          <textarea
+            className="form__input"
+            type="textarea"
+            id="requesterMessage"
+            placeholder="Type Message"
+            style={{ height: '200px' }}
+            onChange={(e) => handleInputChange(e)}
+          />
+        </div>
+      </div>
+      <div class="footer">
+        <button type="submit" class="btn" onClick={() => handleSubmit()}>
+          Submit ticket
+        </button>
+      </div>
+    </div>
+  );
+  /* 
+            <div id="ticket-form">
+              <h1>{this.state.authorized ? 'Contact' : 'Enter the password'}</h1>
+              {this.state.authorized ? contactInfo : login}
+            </div>
+            */
+}
+
+export default TicketForm;
