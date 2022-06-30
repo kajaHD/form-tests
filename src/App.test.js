fb98f9fd5cb6f91d03ac82abfd50e6fcb1506b68
Kaja <k.chade@livechatinc.com>
2022-06-30 14:33:56 +0200

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/create ticket/i);
   expect(linkElement).toBeInTheDocument();
 });
